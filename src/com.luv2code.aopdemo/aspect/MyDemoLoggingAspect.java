8947edfd584f991166fe4f2adc653b57eecb9152
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-07-01 14:53:07 +0300

    AOP: After returning. Modify data

--- src/com.luv2code.aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com.luv2code.aopdemo/aspect/MyDemoLoggingAspect.java
@@ -25,9 +25,28 @@ public class MyDemoLoggingAspect {
         // print out which method we are advising on
         String method = theJoinPoint.getSignature().toShortString();
         System.out.println("\n====>>> result is: " + result);
+        
+        // let's post-process the data ... let's modify
+        
+        // convert the account names to uppercase
+        convertAccountNamesToUpperCase(result);
+
+        System.out.println("\n=====>>> result is:" + result);
 
 
 
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+        // loop through accounts
+
+        for(Account tempAccount : result) {
+            // get uppercase version of name
+            String theUpperName = tempAccount.getName().toUpperCase();
+
+            // update the name on the account
+            tempAccount.setName(theUpperName);
+        }
     }
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
